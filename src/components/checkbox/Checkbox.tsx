8971344236f6ba4b98ee57254146a318c96235ae
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-09-25 15:12:41 +0530

    feat: add checkbox component

---
+++ src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import "./checkbox.scss";
+import { clsx } from "clsx";
+
+type CustomCheckboxProps = {
+  classes?: string;
+  label?: string;
+} & React.ComponentProps<"input">;
+
+const Checkbox = (props: CustomCheckboxProps) => {
+  const { classes, label, checked, ...rest } = props;
+  const defaultState = checked ? checked : false;
+  const [isChecked, setIsChecked] = useState(defaultState);
+
+  return (
+    <div className="checkbox-wrapper">
+      <label className="flex gap-4 items-center cursor-pointer">
+        <input
+          type="checkbox"
+          className={clsx("checkbox-input", classes, { checked: isChecked })}
+          checked={isChecked}
+          onChange={() => setIsChecked((prev) => !prev)}
+          {...rest}
+        />
+        <span className="font-bold">{label}</span>
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
