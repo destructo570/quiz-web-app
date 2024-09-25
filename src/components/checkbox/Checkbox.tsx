5d3343b30616de2a336c9dbddede46032650d57e
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-09-25 22:06:54 +0530

    feat: add custom action for checkbox onchange

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -6,21 +6,39 @@ import { clsx } from "clsx";
 type CustomCheckboxProps = {
   classes?: string;
   label?: string;
+  onCheckboxClick?: (checked: boolean) => void;
 } & React.ComponentProps<"input">;
 
 const Checkbox = (props: CustomCheckboxProps) => {
-  const { classes, label, checked, ...rest } = props;
+  const {
+    classes,
+    label,
+    checked,
+    onCheckboxClick = () => {},
+    ...rest
+  } = props;
   const defaultState = checked ? checked : false;
   const [isChecked, setIsChecked] = useState(defaultState);
 
+  const onClick = (e) => {
+    e?.stopPropagation();
+    setIsChecked((prev) => {
+      onCheckboxClick(!prev);
+      return !prev;
+    });
+  };
+
   return (
-    <div className={clsx("checkbox-wrapper", classes)}>
+    <div
+      className={clsx("checkbox-wrapper cursor-pointer", classes)}
+      onClick={onClick}
+    >
       <label className="flex gap-4 items-center cursor-pointer">
         <input
           type="checkbox"
           className={clsx("checkbox-input", { checked: isChecked })}
           checked={isChecked}
-          onChange={() => setIsChecked((prev) => !prev)}
+          onChange={onClick}
           {...rest}
         />
         <span className="font-bold">{label}</span>
